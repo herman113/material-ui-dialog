a9f00eabcbb12af5d6528fe6d5db57aa2ad9b5bd
Herman Soto <hermansoto@me.com>
2019-11-16 17:44:11 -0500

    Added material-ui with a react hook

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Subscribe from './components/Subscribe'
+
 function App() {
   return (
     <div className="App">
+      <Subscribe open={true}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Subscribe.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Dialog, Button, DialogTitle, DialogContent, DialogActions, IconButton } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
+
+export default function SubscribeModal(props) {
+
+  return (
+    <div>
+      <div>Hello from subscribe modal.</div>
+
+      <Dialog onClose={props.onClose} aria-labelledby="customized-dialog-title" open={props.open}>
+
+      <DialogTitle id="customized-dialog-title" onClose={props.onClose}>
+        Modal title
+      </DialogTitle>
+
+      <DialogContent dividers>
+
+      </DialogContent>
+
+      <DialogActions>
+        <Button autoFocus onClick={props.onClose} color="primary">
+          Save changes
+        </Button>
+      </DialogActions>
+
+    </Dialog>
+  </div>
+  )
+} 
+
+SubscribeModal.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func
+}
